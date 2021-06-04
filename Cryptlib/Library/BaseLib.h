fecf42467edea9a3031990ddd4e9f5d3c6919459
Eniac Zhang <eniacz@hp.com>
2021-06-04 22:03:43 +0000

    initial checkin for 2021 submission

--- Cryptlib/Library/BaseLib.h
+++
@@ -1,9 +0,0 @@
-#include <efi.h>
-#include <efilib.h>
-
-UINT32 WriteUnaligned32 (UINT32 *Buffer, UINT32 Value);
-UINTN AsciiStrSize (CHAR8 *string);
-char *AsciiStrnCpy(char *Destination, char *Source, UINTN count);
-char *AsciiStrCat(char *Destination, char *Source);
-CHAR8 *AsciiStrCpy(CHAR8 *Destination, CHAR8 *Source);
-UINTN AsciiStrDecimalToUintn(const char *String);
--- include/shell.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHIM_SHELL_H
-#define SHIM_SHELL_H
-
-EFI_STATUS
-argsplit(EFI_HANDLE image, int *argc, CHAR16*** ARGV);
-
-#endif /* SHIM_SHELL_H */
